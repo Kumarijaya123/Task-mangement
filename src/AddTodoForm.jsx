040d4adecc047f6abdcf427ae183ef1fef2716b1
Kumari Jaya <jaya28032001@gmail.com>
2023-12-27 17:46:43 +0530

    first commit

---
+++ src/AddTodoForm.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
+import styles from "./AddTodoForm.module.css";
+function AddTodoForm({ addTodo }) {
+    const [todoTitle, setTodoTitle] = useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const newTodo = {
+            completed: false,
+            title: todoTitle,
+            id: uuid(),
+        };
+        addTodo(newTodo);
+        setTodoTitle("");
+    };
+    return (
+        <form onSubmit={handleSubmit} className={styles.todoForm}>
+            <input
+                type="text"
+                name="todoTitle"
+                id="todoTitle"
+                value={todoTitle}
+                onChange={(e) => setTodoTitle(e.target.value)}
+            />
+            <button type="submit"> + Add Todo</button>
+        </form>
+    );
+}
+
+export default AddTodoForm;
